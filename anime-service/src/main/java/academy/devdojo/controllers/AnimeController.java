1d1973c23941c01bafde7e0fdefec064fe3ae070
sks-wrath <skswrath@gmail.com>
2025-02-20 16:55:40 -0300

    video15: exercise 03 PostMapping

---
+++ anime-service/src/main/java/academy/devdojo/controllers/AnimeController.java
@@ -0,0 +1,40 @@
+package academy.devdojo.controllers;
+
+
+import academy.devdojo.domain.Anime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+@RestController
+@RequestMapping("v1/animes")
+@Slf4j
+public class AnimeController {
+
+    @GetMapping
+    public List<Anime> listAll(@RequestParam(required = false) String name) {
+        var animes =  Anime.getAnimes();
+        if (name == null) return animes;
+        return  animes.stream().filter(anime -> anime.getName().equalsIgnoreCase(name)).toList();
+
+    }
+
+    @GetMapping("{id}")
+    public Anime findById(@PathVariable Long id ) {
+        return Anime.getAnimes()
+                .stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst().orElse(null);
+
+    }
+
+    @PostMapping
+    public Anime save(@RequestBody Anime anime ) {
+        anime.setId(ThreadLocalRandom.current().nextLong(100_000));
+        Anime.getAnimes().add(anime);
+        return anime;
+    }
+}
---
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package academy.devdojo.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController()
+@RequestMapping("v1/greetings")
+@Slf4j
+//@RequestMapping(value = "greetings")
+public class HelloController {
+
+    @GetMapping
+    public String hi(){
+        return "OMAE WA MO SHINDE IRU";
+    }
+
+    @PostMapping
+    public Long save (@RequestBody String name){
+        log.info("save '{}'", name);
+        return ThreadLocalRandom.current().nextLong(1, 1000);
+    }
+
+}
